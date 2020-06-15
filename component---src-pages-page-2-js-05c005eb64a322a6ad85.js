1755eaf96c845ba020b2ec4d0ed6691c09682aee
richardjharr87 <66270343+richardjharr87@users.noreply.github.com>
2020-06-15 11:10:00 -0700

    Updates

---
+++ component---src-pages-page-2-js-05c005eb64a322a6ad85.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),c=a("qhky"),o=a("7oih"),r=a("VUD3");t.default=function(){return l.a.createElement(o.a,{pageName:"two"},l.a.createElement(c.a,null,l.a.createElement("title",null,"Page Two")),l.a.createElement(r.a,{type:"content",className:"text-center"},l.a.createElement("h1",null,"Page Two"),l.a.createElement("p",null,"Welcome to page 2")))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-05c005eb64a322a6ad85.js.map
